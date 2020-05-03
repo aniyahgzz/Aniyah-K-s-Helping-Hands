cb8db304a5362023edf5907a199a70a0cc63edd9
aniyahgzz <62959135+aniyahgzz@users.noreply.github.com>
2020-05-03 14:53:39 -0400

    Update homescreen.js

--- src/login/homescreen.js
+++ src/login/homescreen.js
@@ -1,58 +1,35 @@
 import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  Keyboard,
-  View,
-  TextInput,
-  TouchableWithoutFeedback,
-  Alert,
-  KeyboardAvoidingView,
-  Button,
-  Image,
-  TouchableHighlight,
-  ImageBackground,Platform,Animated
-} from 'react-native';
+import {StyleSheet, Text, Keyboard, View, Button, Image,  TouchableHighlight, ImageBackground,} from 'react-native';
 
+import logo from './mylogo.png';
 
+class Home extends React.Component {
 
-class Homescreen extends React.Component {
+render() {
+  return (
 
+<View style={styles.container}>
+
+  <Text style={styles.title}>
+     WELCOME ! Enjoy Your Shopping !
+  </Text>
   
+<View style={styles.center}>
+<Image source = {logo }
+ style={{ height: 100, width: 200, alignItems: 'center', }}/>
+</View>   
 
-  render() {
-    return (
 
+ <TouchableHighlight style={styles.touchableButton1} onPress={() => this.props.navigation.navigate('SignIn')}>
+  <Text style={styles.buttonText}>
+     Next
+  </Text>
 
-      <View style={styles.container}>
-       
-          <View style={styles.textContainer}>
-
-          <Image source ={{uri: 'https://codehs.com/uploads/5bf96b911bce5e9ccc9d9921a10123e9'}}
- style={{ height: 100, width: 250, justifyContent: 'center', }}
-/>
-          
-          
-            </View>
-
+ </TouchableHighlight>
 
  
 
-<View style={styles.textContainer}>
-            <TouchableHighlight
-              onPress={() => this.props.navigation.navigate('File')}
-            >
-           <View style={styles.touchableButton1}>
-                        <Text style={styles.buttonText}>
-                            GET STARTED
-                        </Text>
-                    </View>
-                </TouchableHighlight>
-            </View>
-
- 
-
-      </View>
+</View>
     );
   }
 }
@@ -65,33 +42,7 @@ const styles = StyleSheet.create({
   },
 
 
-  textContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
-
-
-   touchableButton1: {
-        backgroundColor: '#7d0ae6',
-        height: 40,
-        width: 200,
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: 100,
-        marginBottom: 50,
-         borderRadius: 10,
-   
-   
-    },
-   
-    buttonText: {
-     
-        
-        fontSize: 14,
-        fontWeight:'bold',
-    }
 });
 
 
-export default Homescreen;
+export default Home;
