073928203d702e3cbabb3ca133f17617ee5f7fb8
aniyahgzz <62959135+aniyahgzz@users.noreply.github.com>
2020-05-14 22:40:05 -0400

    Update menclothing.js

--- menclothing.js
+++ menclothing.js
@@ -13,12 +13,27 @@ render() {
   <Text style={styles.title}>
      Men's Clothing
   </Text>
-  
-<View style={styles.center}>
-<Image source = { logo }
+
+  <Image source = { logo }
  style={{ height: 100, width: 200, alignItems: 'center', }}/>
-</View>   
- 
+
+<View style={styles.center}>
+
+<Image source={{uri:'https://p.kindpng.com/picc/s/490-4906049_9010-men-clothing-man-stylish-dress-png-transparent.png'}} 
+style={{height:115, width: 115, margin: 5,}}/>
+
+<Image source={{uri:'https://i.dlpng.com/static/png/1491136-t-shirt-suit-stock-photography-clothing-mens-suits-blazer-png-900_500_preview.png'}} 
+style={{height:115, width: 115, margin: 5,}}/>
+
+</View>
+
+<View style={styles.center}>
+<Image source={{uri:'https://image.spreadshirtmedia.com/image-server/v1/products/T23A2PA3495PT17X0Y30D1002944439FS3675/views/1,width=650,height=650,appearanceId=2.jpg'}} 
+style={{height:115, width: 115, margin: 5,}}/>
+
+<Image source={{uri:'https://c7.uihere.com/files/427/516/912/t-shirt-designer-antarctic-men-s-lattice-casual-shirt.jpg'}} 
+style={{height:115, width: 115, margin: 5,}}/>
+</View>
 
  <TouchableHighlight style={styles.touchableButton1} onPress={() => this.props.navigation.navigate('SignIn')}>
   <Text style={styles.buttonText}>
@@ -35,7 +50,6 @@ render() {
  </TouchableHighlight>
 
  
-
 </View>
     );
   }
@@ -46,8 +60,13 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     justifyContent: 'center',
+    alignItems: 'center',
   },
-
+  center: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    margin:5,
+  }
 
 });
 
