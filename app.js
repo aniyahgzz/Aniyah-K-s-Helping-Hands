6e4fe8ad4ee519cb9c7ce35a6d2539101572f59c
aniyahgzz <62959135+aniyahgzz@users.noreply.github.com>
2020-05-14 22:36:37 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, Keyboard,  View, TextInput, TouchableWithoutFeedback, Alert, KeyboardAvoidingView, Platform,Button, Image, TouchableHighlight,} from 'react-native';
 import { createStackNavigator } from 'react-navigation';
-import Homescreen from './src/login/homescreen.js';
-import Email from './src/login/email.js';
-import File from './src/login/newfile.js';
-
-
-
-
-
+import Home from './components/home.js';
+import SignIn from './components/signin.js';
+import Men from './components/menClothing.js';
+import Accessories from './components/accessories.js';
+import Shoes from './components/shoes.js';
+import Women from './components/womenJeans.js';
+import Jackets from './components/jackets.js';
+import ClosingPage from './components/closingPage.js';
 
 const RootStack = createStackNavigator(
 {
-  Home: { screen: Homescreen },
-  Email: {screen: Email},
-  File: {screen: File},
-  
+  Home: { screen: Home },
+  SignIn: { screen: SignIn },
+  Mens: { screen: Men },
+  Accessories: { screen: Accessories },
+  Shoes: { screen: Shoes },
+  Women: { screen: Women },
+  Jackets: { screen: Jackets },
+  ClosingPage: { screen: ClosingPage },  
+
 },
 {
-    initialRouteName: 'Home',
+  initialRouteName: 'Home',
 }
 
 );
@@ -28,4 +33,6 @@ export default class App extends Component {
     return <RootStack />;
   }
 }
+
+
 
