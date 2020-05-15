bed6edd6bd571402c15cc83650faa9a0ab6371cc
aniyahgzz <62959135+aniyahgzz@users.noreply.github.com>
2020-05-14 22:42:18 -0400

    Update womenJeans.js

--- womenJeans.js
+++ womenJeans.js
@@ -19,6 +19,21 @@ render() {
  style={{ height: 100, width: 200, alignItems: 'center', }}/>
 </View>   
 
+<View style={styles.center}>
+<Image source={{uri:'https://i.pinimg.com/736x/ae/b9/94/aeb9942180c9a5b35f71e0108e5d8e41.jpg'}} 
+style={{height:110, width: 110, margin: 5,}}/>
+
+<Image source={{uri:'https://png.pngitem.com/pimgs/s/7-71904_denim-skinny-blue-jeans-womens-hd-png-download.png'}} 
+style={{height:110, width: 110, margin: 5,}}/>
+</View>
+
+<View style={styles.center}>
+<Image source={{uri:'https://toppng.com/uploads/preview/ripped-jeans-png-trendy-ripped-jeans-for-wome-11563008442nexteelelb.png'}} 
+style={{height:110, width: 110, margin: 5,}}/>
+
+<Image source={{uri:'https://www.kindpng.com/picc/m/242-2423761_womens-jeans-png-pocket-transparent-png.png'}} 
+style={{height:110, width: 110, margin: 5,}}/>
+</View>
 
  <TouchableHighlight style={styles.touchableButton1} onPress={() => this.props.navigation.navigate('Shoes')}>
   <Text style={styles.buttonText}>
@@ -46,8 +61,13 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     justifyContent: 'center',
+    alignItems: 'center',
   },
-
+  center: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    margin:5,
+  }
 
 });
 
